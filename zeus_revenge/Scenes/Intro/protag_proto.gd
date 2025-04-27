13fa8c763c571277f278ad58e3001629d11e6467
Michael G. <95585542+Zaphryx1@users.noreply.github.com>
2025-04-26 21:50:36 -0600

    minotaur under MONSTERS

---
+++ zeus_revenge/Scenes/Intro/protag_proto.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+
+
+var speed = 100
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("move_right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("move_left"):
+		input_vector.x -= 1
+	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("move_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("move_up"):
+		input_vector.y -= 1
+
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector.normalized() * speed
+		move_and_slide()
+
+		# Play correct walking animation
+		if abs(input_vector.x) > abs(input_vector.y):
+			if input_vector.x > 0:
+				sprite.play("walk right")
+			else:
+				sprite.play("walk left")
+		else:
+			if input_vector.y > 0:
+				sprite.play("walk forward")
+			else:
+				sprite.play("walk back")
+	else:
+		velocity = Vector2.ZERO
+		sprite.play("default")  # Play idle animation or stop
