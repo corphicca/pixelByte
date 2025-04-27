f00e8c52a837957ba7859379d627d83410bda87e
J0nA-1 <lg211874@gmail.com>
2025-04-27 05:08:04 -0600

    menu finished

--- zeus_revenge/Scenes/Menu/menu scripts/intro_menu.gd
+++ zeus_revenge/Scenes/Menu/menu scripts/intro_menu.gd
@@ -1,30 +1,31 @@
 extends Control
 
-@onready var crashing = $Sprite2D/CrashIn
+@onready var crashing = $Shaker/Sprite2D/CrashIn
 @onready var leave = $ColorRect/Back_out
-
-@export var randomStrength: float = 30.0
-@export var shakeFade: float = 5.0
-
-var rng = RandomNumberGenerator.new()
-
-var shake_strength: float = 0.0
-
-func apply_shake():
-	shake_strength = randomStrength
-	
-func randomOffset() -> Vector2:
-	return Vector2(rng.randf_range(-shake_strength,shake_strength),rng.randf_range(-shake_strength,shake_strength))
+@onready var shaker = $Shaker # This will be a new Node we'll add
+@onready var shake_timer = Timer.new()
 
 func _ready():
 	crashing.play("StarCrash")
 	leave.play("leave")
+	$CenterContainer/VBoxContainer/boxIn.play("boxIn")
 	%Play.pressed.connect(play)
 	%Settings.pressed.connect(settings)
 	%ExitGame.pressed.connect(exitGame)
 
+	# Set up shake timer
+	shake_timer.wait_time = 2.02
+	shake_timer.one_shot = true
+	shake_timer.timeout.connect(start_shake)
+	add_child(shake_timer)
+	shake_timer.start()
+
+func start_shake():
+	# Simple shake by randomly moving the "shaker" node
+	shaker.start_shaking()
+
 func play():
-	get_tree().change_scene_to_file("res://Scenes/Intro/MotherIntro.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Intro/story.tscn")
 
 func settings():
 	get_tree().change_scene_to_file("res://Scenes/Menu/settings.tscn")
