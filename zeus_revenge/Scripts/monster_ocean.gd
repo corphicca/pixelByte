55e33d3990021057a6c3953fdd69dfb805570716
JuanReynaPacheco <oo.jr54@gmail.com>
2025-04-27 09:30:20 -0600

    Enemy Update

--- zeus_revenge/Scripts/monster_ocean.gd
+++ zeus_revenge/Scripts/monster_ocean.gd
@@ -4,14 +4,22 @@ extends CharacterBody2D
 @onready var timer = $Timer
 @onready var pos = $"."
 @onready var vis = $VFX
+@onready var Attck_time = $attack
 var vfx;
 
 func _ready() -> void:
 	timer.start()
-	vfx = vis.get_node("AnimatedSprite2D2")
+	vfx = character.get_node("AnimatedSprite2D2")
 
 func _on_timer_timeout() -> void:
 	var distance = character.global_position.distance_to(pos.global_position)
 	if distance <= 250:
-		vfx.visibile = true
+		vfx.show()
 		vfx.play("Aim")
+		Attck_time.start()
+		timer.stop()
+
+func _on_attack_timeout() -> void:
+	vfx.hide()
+	Attck_time.stop()
+	timer.start()
