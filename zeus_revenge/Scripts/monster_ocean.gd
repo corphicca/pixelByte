5e14330f34e222d2481c06a67ab8656f046a7576
J0nA-1 <lg211874@gmail.com>
2025-04-27 10:25:08 -0600

    asd

--- zeus_revenge/Scripts/monster_ocean.gd
+++ zeus_revenge/Scripts/monster_ocean.gd
@@ -2,42 +2,33 @@ extends CharacterBody2D
 
 @onready var character = $"../../CharacterBody2D"
 @onready var timer = $Timer
-@onready var pos = $"."
+@onready var pos = $"."  # This refers to the current node
 @onready var vis = $VFX
-@onready var Attck_time = $attack
-var vfx;
+@onready var attack_time = $attack  # Fix variable name to snake_case (Godot style)
+var vfx
 
 func _ready() -> void:
 	timer.start()
 	vfx = character.get_node("AnimatedSprite2D2")
 
 func _on_timer_timeout() -> void:
-<<<<<<< HEAD
-	print("Attack")
-	
-=======
 	var distance = character.global_position.distance_to(pos.global_position)
 	if distance <= 250:
 		vfx.show()
 		vfx.play("Aim")
-		Attck_time.start()
+		attack_time.start()
 		timer.stop()
 
 func _on_attack_timeout() -> void:
 	vfx.hide()
-	Attck_time.stop()
+	attack_time.stop()
 	vis.show()
 	vis.global_position = character.global_position
 	vis.play("water")
-	vis.animation_finished
 	timer.start()
-<<<<<<< HEAD
 
 func _on_vfx_animation_finished() -> void:
 	vis.hide()
 
 func _on_kill_zone_body_entered(body: Node2D) -> void:
 	print("Player damaged")
-=======
->>>>>>> 0a93c55856157b0920fb3afa036ed314d77fbb7b
->>>>>>> dcf9d54980e59880658544f432f913d07276d734
