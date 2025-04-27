a955dc9313e06619147774f5b52eddc73610c826
JuanReynaPacheco <oo.jr54@gmail.com>
2025-04-27 10:23:20 -0600

    22
    222

--- zeus_revenge/Scripts/monster_ocean.gd
+++ zeus_revenge/Scripts/monster_ocean.gd
@@ -12,10 +12,8 @@ func _ready() -> void:
 	vfx = character.get_node("AnimatedSprite2D2")
 
 func _on_timer_timeout() -> void:
-<<<<<<< HEAD
 	print("Attack")
 	
-=======
 	var distance = character.global_position.distance_to(pos.global_position)
 	if distance <= 250:
 		vfx.show()
@@ -31,13 +29,9 @@ func _on_attack_timeout() -> void:
 	vis.play("water")
 	vis.animation_finished
 	timer.start()
-<<<<<<< HEAD
 
 func _on_vfx_animation_finished() -> void:
 	vis.hide()
 
 func _on_kill_zone_body_entered(body: Node2D) -> void:
 	print("Player damaged")
-=======
->>>>>>> 0a93c55856157b0920fb3afa036ed314d77fbb7b
->>>>>>> dcf9d54980e59880658544f432f913d07276d734
