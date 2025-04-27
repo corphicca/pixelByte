62e3565029b1e08a225deacec709a07178764d33
JuanReynaPacheco <oo.jr54@gmail.com>
2025-04-27 09:18:25 -0600

    Enemy Stuff
    MEssed around with enemies

--- zeus_revenge/Scripts/monster_ocean.gd
+++ zeus_revenge/Scripts/monster_ocean.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
-@onready var character = $"../CharacterBody2D"
+@onready var character = $"../../CharacterBody2D"
 @onready var timer = $Timer
+@onready var pos = $"."
+@onready var vis = $VFX
+var vfx;
 
 func _ready() -> void:
 	timer.start()
+	vfx = vis.get_node("AnimatedSprite2D2")
 
 func _on_timer_timeout() -> void:
-	print("Attack")
-	print(character.position)
+	var distance = character.global_position.distance_to(pos.global_position)
+	if distance <= 250:
+		vfx.visibile = true
+		vfx.play("Aim")
